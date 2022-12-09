a8ad826f8e99ff6c066b01fc5cab0c0cec6d8e76
Rowan Katekar <rjkat@ieee.org>
2022-12-09 23:02:48 +1100

    day 7 part 2

--- advent-2022-day07.sh
+++ advent-2022-day07.sh
@@ -2,7 +2,8 @@
 
 FAKE_DIR=day07-temp
 
-sed 's/\$ ls//g' \
+cat day07.txt \
+| sed 's/\$ ls//g' \
 | sed '/^$/d' \
 | sed -E 's/^([0-9]+) (.*)/dd if=\/dev\/zero bs=\1 count=1 of=\2/g' \
 | sed -E 's/^dir (.*)/mkdir -p \1/' \
@@ -19,4 +20,24 @@ for i in `seq $max_depth`; do
     sizes+="$tree"
     sizes+=$'\n'
 done
-echo -n "$sizes" | sed '/^$/d' | perl -e '$\ = "\n"; %sizes=(); while (<>) { @toks = split; $sizes{@toks[1]} += @toks[0] }; $total = 0; foreach (values %sizes) { $total += $_ unless $_ > 100000; }; print $total'
+
+echo "Part 1"
+echo -n "$sizes" \
+    | sed '/^$/d' \
+    | perl -e '%sizes=(); while (<>) { @toks = split; $sizes{@toks[1]} += @toks[0] }; $total = 0; foreach (values %sizes) { $total += $_ unless $_ > 100000; }; print $total, "\n"'
+
+echo "Part 2"
+
+toplevel=`echo -n "$sizes" | sed 's/.*\/.*\/.*//g' | sed '/^$/d'`
+toplevel+=$'\n'
+toplevel+=`find ${FAKE_DIR} -type f -depth 1 | xargs wc -c`
+
+needed=`echo "$toplevel" \
+    | perl -e '$used = 0; while (<>) { @toks = split; $used += @toks[0]; }; $needed = 30000000 - (70000000 - $used); print $needed'`
+
+echo -n "$sizes" \
+    | sed '/^$/d' \
+    | perl -e "%sizes=(); while (<>) { @toks = split; \$sizes{@toks[1]} += @toks[0] }; foreach (keys %sizes) { \$sizes{\$_} >= $needed && print \$_, \" \", \$sizes{\$_}, \"\\n\"  }" \
+    | sort -n -k 2 \
+    | head -n 1
+
